507eee3a9afd89fd283f99a8ef1311359877278a
Cristobal Henriquez <brohenriquez@gmail.com>
2025-03-07 12:00:49 -0500

    W01-activity-04 (EJS View Engine) complete

--- server.js
+++ server.js
@@ -6,9 +6,17 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayout = requrie(express-ejs-layouts)
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layout/layout") // not at view root
 
 /* ***********************
  * Routes
