fb4b91e3e13449e1fe9f5e6424f438b7a6608411
Cristobal Henriquez <brohenriquez@gmail.com>
2025-03-27 17:22:10 -0400

    corrected ssl

--- database/index.js
+++ database/index.js
@@ -6,39 +6,32 @@ require('dotenv').config();
  * But will cause problems in production environment
  * If - else will make determination which to use
  * *************** */
-let pool;
-
-if (process.env.NODE_ENV === "development") {
+let pool
+if (process.env.NODE_ENV == "development") {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false,
     },
-  });
-} else {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    // Configure SSL as needed for your production provider
-    ssl: {
-      // For some providers like Render, you might still need:
-      rejectUnauthorized: true,
-      // Or if using a trusted certificate:
-      // rejectUnauthorized: true,
-    },
-  });
-}
+})
 
+// Added for troubleshooting queries
+// during development
 module.exports = {
   async query(text, params) {
     try {
-      const res = await pool.query(text, params);
-      if (process.env.NODE_ENV === "development") {
-        console.log("executed query", { text });
-      }
-      return res;
+      const res = await pool.query(text, params)
+      console.log("executed query", { text })
+      return res
     } catch (error) {
-      console.error("error in query", { text });
-      throw error;
+      console.error("error in query", { text })
+      throw error
     }
   },
-};
+}
+} else {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  })
+  module.exports = pool
+}
